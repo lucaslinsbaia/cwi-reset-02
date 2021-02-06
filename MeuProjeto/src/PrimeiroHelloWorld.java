f9408203f71044c7eb000d4899f08becc394ba22
Lucas Lins Baía <lucaslinsbaia@gmail.com>
2021-02-06 12:38:12 -0300

    Primeiros arquivos a subir

---
+++ MeuProjeto/src/PrimeiroHelloWorld.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class PrimeiroHelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo! Sofri mas consegui!!!");
+
+    }
+}

61687d1f0731d60894753a6d51391da57115315c
Lucas Lins Baía <lucaslinsbaia@gmail.com>
2021-02-06 15:22:19 -0300

    Instruções de Comentários

--- MeuProjeto/src/PrimeiroHelloWorld.java
+++ MeuProjeto/src/PrimeiroHelloWorld.java
@@ -1,8 +1,13 @@
-import java.sql.SQLOutput;
-/* Comentário de teste adicionado
-* */
+
+/* Comentário de mais
+de uma linha adicionado */
+
+
+
 public class PrimeiroHelloWorld {
     public static void main(String[] args) {
+        //Sistema de comentários igual ao JS
+        //Aqui imprimimos um texto
         System.out.print("Troquei a mensagem mais uma vez!");
 
     }

e8f30aca35b7913c90be79579c13044c905795f1
Lucas Lins Baía <lucaslinsbaia@gmail.com>
2021-02-10 19:21:42 -0300

    Push do EXERCICIO 2 da AULA 1

---
+++ AULA01/EXERCICIO02/src/Aplicacao.java
@@ -0,0 +1,9 @@
+public class Aplicacao {
+    public static void main(String[] args) {
+        Comparador comparar = new Comparador();
+    boolean comparar1 = comparar.menorQue(5,7);
+        System.out.println("A resposta é "+ comparar1);
+
+    }
+
+}
---
+++ AULA01/EXERCICIO02/src/Comparador.java
@@ -0,0 +1,9 @@
+public class Comparador {
+    public boolean menorQue(int valor1, int valor2) {
+        if (valor1 < valor2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
